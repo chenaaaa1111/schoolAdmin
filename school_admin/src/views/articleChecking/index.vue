2311f7e06fe46d242099e260e7a54cfd8a856d05
chenaaaa1111 <934471808@qq.com>
2019-12-28 01:22:48 +0800

    解决冲突

---
+++ school_admin/src/views/articleChecking/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <MainTabContent></MainTabContent>
+    </div>
+</template>
+<script>
+    import MainTabContent from "@/components/TabsCompnents.vue"
+
+    export default {
+        data() {
+            return {
+                showArticle: true,
+                showView: false
+            }
+        },
+        components: {
+            MainTabContent
+        }
+    }
+</script>
